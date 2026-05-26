c9ef14713d4400a012a123f50199266d2bc1c4a6
Lahar <kukrejalc@rknec.edu>
2026-05-26 15:33:08 +0530

    recent update

--- src/main/java/controller/TestController.java
+++ src/main/java/controller/TestController.java
@@ -1,17 +1,14 @@
-package com.lahar.hms.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+package com.lahar.hms;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:3000")
 public class TestController {
 
-    @GetMapping("/test")
-    public String test() {
-        return "Backend is working!";
+    @GetMapping("/")
+    public String home() {
+        return "HMS Backend is Running 🚀";
     }
 }
